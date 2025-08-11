cf1022019e1c89874017fca082b638e309ca5044
kesly_13 <keslyotero@gmail.com>
2025-08-11 11:42:28 -0500

    mi primer commit

---
+++ back/src/controllers/crud.controller.js
@@ -0,0 +1,59 @@
+const db = require ('../config/conexion_DB')
+
+
+class crudController {
+
+    //obtener los registros de una tabla
+    async obtenerTodos(tablas){
+        const [resultados] = await db.query(`SELECT * FROM ${tabla}`);
+        return resultados;      
+    }
+//obtener un registro po ID
+async obtenerUno(tabla, idCampo, id){
+    try {
+        const [resultado] = await db.query(`SELECT * ?? = ?`,[tabla, idCampo, id]);
+        return resultado[0];
+    }catch(error) {
+        throw error;
+    }
+}
+
+//crea un nuevo registro
+async crear(tabla, data){
+    try{
+    const[resultado] = await db.query(`INSERT INTO ?? SET ?`,[tabla, data]);
+        return {...data, id:resultado.insertId };
+    }catch(error) {
+        throw error;
+    }
+}
+
+//actualizar un registro por id
+async actualizar(tabla, idCampo, id, data){
+    try{
+         const[resultado] = await db.query(`UPDATE  ?? SET ? WHERE ?? = ?`,[tabla, data, idCampo, id]);
+         if(resultado.affectedRows === 0) {
+            throw new Error('registro no encontrado');
+         }
+        return await this.obtenerUno(tabla, idCampo, id);
+        }catch(error) {
+        throw error;
+        }
+    }
+//Eliminar un registro por ID 
+async eliminar(tabla, idCampo, id){
+    try{
+         const[resultado] = await db.query(`DELETE FROM  ?? WHERE ?? = ?`,[tabla, idCampo, id]);
+         if(resultado.affectedRows === 0) {
+            throw new Error('registro no encontrado');
+         }
+        return {message: 'registro eliminado exitosamente'}
+        }catch(error) {
+        throw error;
+    }
+    }
+
+}
+ 
+
+module.exports = crudController;
